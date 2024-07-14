91d627b34732dcf61c7a177be81ff409e5fb6a1b
aucalloni <acalloni@ivytech.edu>
2024-07-13 20:01:54 -0400

    Test Merge

--- Controllers/previous_week_controller.py
+++ Controllers/previous_week_controller.py
@@ -13,6 +13,11 @@ class PreviousWeekController:
     def update_entry(self, record_id, sign_in_time, sign_out_time, additional_notes):
         self.model.update_entry(record_id, sign_in_time, sign_out_time, additional_notes)
 
+        # Same logic to handle validating if our update method worked correctly.
+        updated_entry = self.model.get_entry_by_id(record_id)
+        return updated_entry[3] == sign_in_time and updated_entry[4] == sign_out_time and updated_entry[
+            5] == additional_notes
+
     def get_entries_for_week(self, start_date, end_date):
         return self.model.get_entries_for_week(start_date, end_date)
 
@@ -21,3 +26,7 @@ class PreviousWeekController:
 
     def clear_entries(self):
         self.model.clear_entries()
+
+    # Separate method to handle getting a single record. Same logic as our current week model.
+    def get_entry_by_id(self, record_id):
+        return self.model.get_entry_by_id(record_id)
