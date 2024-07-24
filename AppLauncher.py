14e0e01cbf683e2e4094dff3ce695135355e6411
AuCalloni <acalloni@ivytech.edu>
2024-07-24 18:56:51 -0400

    Fixed an issue with the global variables not being defined at the module level. This was just to shut my IDE up.

--- AppLauncher.py
+++ AppLauncher.py
@@ -8,7 +8,13 @@ from Views.PreviousWeekGUI_logic import PreviousWeekGUI
 from Views.AppGUI_logic import AppGUIWindow
 import sys
 
+# Define the global variables at the module level so my IDE will shut up.
+previous_week_view = None
+main_view = None
 
+
+# To avoid circular dependency, we have two main call back methods with global variables that handle what view
+# is to be shown. This one calls our previous week's view.
 def switch_to_previous_week_view(associate_controller, current_week_controller, date_transfer_controller,
                                  previous_week_controller):
     global previous_week_view
@@ -17,6 +23,7 @@ def switch_to_previous_week_view(associate_controller, current_week_controller, 
     previous_week_view.show()
 
 
+# This method switches back to our main view.
 def switch_to_main_view(associate_controller, current_week_controller, date_transfer_controller,
                         previous_week_controller):
     global main_view
@@ -25,18 +32,21 @@ def switch_to_main_view(associate_controller, current_week_controller, date_tran
     main_view.show()
 
 
+# Our driving method to launch the application.
 def launch_app():
     # Declare our application
     app = QApplication(sys.argv)
     # Set up our controllers
     db_path = 'TimesRecord.db'
+    # Assign our controllers
     date_transfer_controller = DataTransferController(db_path)
+    # Check if it's a new week, if so perform updates
     date_transfer_controller.check_and_update_week()
     associate_controller = AssociateController(db_path)
     current_week_controller = CurrentWeekController(db_path)
     previous_week_controller = PreviousWeekController(db_path)
 
-    # Assign our window variable the APPGuiWindow class. That class handles calling the UI logic.
+    # Switch to the main view.
     switch_to_main_view(associate_controller, current_week_controller, date_transfer_controller,
                         previous_week_controller)
 
